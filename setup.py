8c0496c2d6d71a0f50a849ec7c4c78b22a6469c5
Kimihiro N <kimihiro.nisimura@gmail.com>
2018-01-24 09:54:01 +0900

    Set version to 0.1.2.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from pip.req import parse_requirements
 
 setup(
     name='ecs_manager',
-    version='0.1.2.7',
+    version='0.1.2.8',
     url='https://github.com/pistatium/ecs_service_manager',
     author='pistatium',
     description='Manage ecs service',
